6b6ecf97acacae6157c93c99594f434423850a76
Serhii-Yakimtso <serafyso@gmail.com>
2021-03-18 20:01:57 +0200

    step 1 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,94 @@
+import React, { Component } from "react";
+
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class Feedback extends Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+
+  handleCkick = (e) => {
+    // console.log("button: ", e.target.name);
+    this.setState((prev) => ({ [e.target.name]: prev[e.target.name] + 1 }));
+    // console.log("state ", this.state);
+  };
+
+  render() {
+    const { state, handleCkick } = this;
+
+    // console.log("state ", state);
+    // console.log("state.good ", state.good);
+    // console.log("state.neutral ", state.neutral);
+    // console.log("state.bad ", state.bad);
+
+    return (
+      <>
+        <section className="feedback">
+          <h2 className="title">Please leave feedback</h2>
+
+          <ul className="feedbackList">
+            <li className="feedbackItem">
+              <button className="feedbackBtn" name="good" onClick={handleCkick}>
+                good
+              </button>
+            </li>
+            <li className="feedbackItem">
+              <button
+                className="feedbackBtn"
+                name="neutral"
+                onClick={handleCkick}
+              >
+                neutral
+              </button>
+            </li>
+            <li className="feedbackItem">
+              <button className="feedbackBtn" name="bad" onClick={handleCkick}>
+                bad
+              </button>
+            </li>
+          </ul>
+        </section>
+
+        <section className="statistics">
+          <h2 className="title">Statistics</h2>
+
+          <ul className="statisticsList">
+            <li className="statisticsItem">Good: {state.good}</li>
+            <li className="statisticsItem">Neutral: {state.neutral}</li>
+            <li className="statisticsItem">Bad: {state.bad}</li>
+          </ul>
+        </section>
+      </>
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+// <h2 className="title">Please leave feedback</h2>
+
+// <ul className="feedbackList">
+//   <li className="feedbackItem">
+//     <button className="feedbackBtn">good</button>
+//   </li>
+//   <li className="feedbackItem">
+//     <button className="feedbackBtn">neutral</button>
+//   </li>
+//   <li className="feedbackItem">
+//     <button className="feedbackBtn">bad</button>
+//   </li>
+// </ul>
+
+// <h2 className="title">Statistics</h2>
+
+// <ul className="statisticsList">
+//   <li className="statisticsItem">good</li>
+//   <li className="statisticsItem">neutral</li>
+//   <li className="statisticsItem">bad</li>
+//   <class></class>
+// </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+export default Feedback;
